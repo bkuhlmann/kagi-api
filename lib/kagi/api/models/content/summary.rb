80ddf5861b2cbc58951421ebbd1f70349b0eb4a4
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-19 16:03:26 -0600

    Added summary content model
    Necessary to model summary content.

---
+++ lib/kagi/api/models/content/summary.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Kagi
+  module API
+    module Models
+      module Content
+        # Models summary data.
+        Summary = Data.define :output, :tokens
+      end
+    end
+  end
+end
