b2dfccff52c6471613d5f49de6f89d0991f0fa9a
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-20 17:06:09 -0600

    Added reference content model
    Necessary to model reference content. This is part of the Fast GPT endpoint.

---
+++ lib/kagi/api/models/content/reference.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Kagi
+  module API
+    module Models
+      module Content
+        # Models reference data.
+        Reference = Data.define :title, :snippet, :url
+      end
+    end
+  end
+end
